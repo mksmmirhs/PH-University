aff30ea03c7b154ac760acce0013b5e008eef7fc
mksmmirhs <meraj.exe@gmail.com>
2023-12-02 17:26:56 +0600

    academic semester create validation

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,55 +1,42 @@
 import { Schema, model } from 'mongoose';
+import { TAcademicSemester } from './academicSemester.interface';
 import {
-  TAcademicSemester,
-  TAcademicSemesterCode,
-  TAcademicSemesterName,
-  TMonths,
-} from './academicSemester.interface';
+  Month,
+  AcademicSemesterCode,
+  AcademicSemesterName,
+} from './academicSemester.constant';
 
-const Month: TMonths[] = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-const SemesterName: TAcademicSemesterName[] = ['Autumn', 'Summar', 'Fall'];
-const SemesterCode: TAcademicSemesterCode[] = ['01', '02', '03'];
-
-const academicSemesterSchema = new Schema<TAcademicSemester>({
-  name: {
-    type: String,
-    enum: SemesterName,
-    required: true,
+const academicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      enum: AcademicSemesterName,
+      required: true,
+    },
+    code: {
+      type: String,
+      enum: AcademicSemesterCode,
+      required: true,
+    },
+    year: {
+      type: Date,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      enum: Month,
+      required: true,
+    },
+    endMonth: {
+      type: String,
+      enum: Month,
+      required: true,
+    },
   },
-  code: {
-    type: String,
-    enum: SemesterCode,
-    required: true,
-  },
-  year: {
-    type: Date,
-    required: true,
-  },
-  startMonth: {
-    type: String,
-    enum: Month,
-    required: true,
-  },
-  endMonth: {
-    type: String,
-    enum: Month,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const AcademicSemester = model<TAcademicSemester>(
   'AcademicSemester',
--- src/app/modules/academicSemester/academicSemister.validation.ts
+++ src/app/modules/academicSemester/academicSemister.validation.ts
@@ -1,12 +1,18 @@
 import { z } from 'zod';
+import {
+  AcademicSemesterCode,
+  AcademicSemesterName,
+  Month,
+} from './academicSemester.constant';
 
 const createAcademicSemesterValidationSchema = z.object({
-  password: z
-    .string({
-      invalid_type_error: 'Password must be string',
-    })
-    .max(20, { message: 'Password can not be more than 20 characters' })
-    .optional(),
+  body: z.object({
+    name: z.enum([...AcademicSemesterName] as [string, ...string[]]),
+    year: z.date(),
+    code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
+    startMonth: z.enum([...Month] as [string, ...string[]]),
+    endtMonth: z.enum([...Month] as [string, ...string[]]),
+  }),
 });
 
 export const AcademicSemesterValidation = {
