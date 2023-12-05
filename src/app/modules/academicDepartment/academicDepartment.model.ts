57c9aed8c4fa32e325687f4b486b66707acf9c4f
mksmmirhs <meraj.exe@gmail.com>
2023-12-05 16:01:44 +0600

    Academic Department interface , model , validation

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+import { TAcademicDepartment } from './academicDepartment.interface';
+
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.ObjectId,
+      required: true,
+      ref: 'AcademicFaculty',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const AcademicDepartment = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema
+);
--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,13 +1,17 @@
 import z from 'zod';
 
 const createAcademicFacultySchemaValidation = z.object({
-  name: z.string({
-    invalid_type_error: 'Faculty name must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Faculty name must be string',
+    }),
   }),
 });
 const updateAcademicFacultySchemaValidation = z.object({
-  name: z.string({
-    invalid_type_error: 'Faculty name must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Faculty name must be string',
+    }),
   }),
 });
 
