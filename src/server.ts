007cea039b3e4c5ecbdc1207f286e537f990ed39
mksmmirhs <meraj.exe@gmail.com>
2023-12-07 01:42:02 +0600

    unhandledRejection and uncaughtException error handle

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
+import { Server } from 'http';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
@@ -15,3 +18,19 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log('😈 unhandledRejection detected Server is shutting down ....');
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log('😈 uncaughtException detected Server is shutting down ....');
+  process.exit(1);
+});
