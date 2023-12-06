196ba1f44d8ce1d479c7d8dbe451ca29ba12dcb6
mksmmirhs <meraj.exe@gmail.com>
2023-12-06 21:15:23 +0600

    zod validation request error handle

---
+++ src/app/interface/error.ts
@@ -0,0 +1,1 @@
+export type TErrorSource = { path: string | number; message: string }[];
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,20 +1,50 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-import { Response, Request, NextFunction } from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ErrorRequestHandler } from 'express';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
+import { ZodError, ZodIssue } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction
-) => {
-  const code = err.statusCode || 500;
-  const message = err.message || 'oops something went wrong';
-  return res.status(code).json({
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'oops something went wrong';
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  const handleZodError = (error: ZodError) => {
+    const statusCode = 400;
+
+    const errorSource: TErrorSource = error.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSource,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSource;
+    statusCode = simplifiedError.statusCode;
+  }
+
+  return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSource,
+    stack: config.APP_ENV === 'Development' ? err.stack : null,
   });
 };
 
