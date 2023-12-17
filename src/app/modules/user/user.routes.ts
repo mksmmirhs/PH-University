04aa12fe51245836d6cca9e36addbf0ad7314e56
mksmmirhs <meraj.exe@gmail.com>
2023-12-17 13:23:46 +0600

    course created

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { UserController } from './user.controller';
 import validateRequest from '../../middleware/validateRequest';
 import { studentValidation } from '../student/student.validation';
+import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
+import { createAdminValidationSchema } from '../Admin/admin.validation';
 
 const router = express.Router();
 
@@ -9,6 +11,17 @@ router.post(
   '/create-student',
   validateRequest(studentValidation.createStudentValidationSchema),
   UserController.createStudent
+);
+router.post(
+  '/create-faculty',
+  validateRequest(createFacultyValidationSchema),
+  UserController.createFaculty
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserController.createAdmin
 );
 
 export const UserRoutes = router;
